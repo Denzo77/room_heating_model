9c2c677d97711b9ac1b5e30010f9efd94e9cea03
Denzo77 <denzo77@users.noreply.github.com>
2017-02-01 16:34:53 +0000

    debugging

--- room_model2.py
+++ room_model2.py
@@ -13,17 +13,17 @@ stored_heat = start_heat + (in_heat + out_heat) [Q_room = Q_start + Q_rad + Q_lo
 
 # Simulation setup. SET THESE VALUES.
 N_ITERATIONS = 4 * 60 * 60  # on for 20 minutes
-START_TEMP = 15.0  # [C]
+START_TEMP = 20.0  # [C]
 OUTSIDE_TEMP = 0.0  # [C]
 RADIATOR_TEMP = [60.0 if (x < 20 * 60) else None for x in range(N_ITERATIONS)]  # [C]
-print(RADIATOR_TEMP)
 ROOM_SIZE = (3.0, 5.0, 2.3)  # w, l, h. Assumes room is cuboid. [m]
 
 # The R(SI) value of the room (resistance to heat transfer).
 # Equivalent of 1/U-value. Using R value as easier to combine.
 # From U-value of 0.8, given by DHD as typical for poorly insulated house.
+# R_WALLS = 0.1  # [K m^2/W]
 R_WALLS = 1.25  # [K m^2/W]
-# R_WALLS = 10  # [K m^2/W]
+# R_WALLS = 10.0  # [K m^2/W]
 
 # Physical constants.
 DENSITY_AIR = 1.205  # Density of air at 20 C, 1 Atm. [Kg/m^3]
@@ -38,11 +38,11 @@ VOLUME = ROOM_SIZE[0] * ROOM_SIZE[1] * ROOM_SIZE[2]  # Volume of room. [m^3]
 
 C_AIR = DENSITY_AIR * VOLUME * Cp_AIR  # Heat capacity of air in the room. [J/K]
 # C_AIR = DENSITY_AIR * VOLUME * Cp_AIR + 40000 # Naively adding thermal capacitance to represent extra objects fails.
-WALL_CONDUCTANCE = 1.0 / (R_WALLS * SURFACE_AREA)  # Thermal conductance of room walls. Equivalent to U-value [W/K]
+WALL_CONDUCTANCE = SURFACE_AREA * (1.0 / R_WALLS)  # Thermal conductance of room walls. Equivalent to U-value [W/K]
 
 
 print("Surface Area: {0:.2f} m^2\nVolume: {1:.2f} m^3".format(SURFACE_AREA, VOLUME))
-print("Thermal Conductance: {0:.2f} W/K\nHeat Capacity: {1:.2f} kJ/K".format(WALL_CONDUCTANCE, C_AIR / 1000.0))
+print("Thermal Conductance: {0:.0f} W/K\nHeat Capacity: {1:.0f} kJ/K".format(WALL_CONDUCTANCE, C_AIR / 1000.0))
 
 
 def heat_in_radiator(room_temp, radiator_temp):
@@ -83,8 +83,8 @@ def calc_temp(room_temp, *args):
 
 
 def foo(room_temp, foo_temp):
-    conductance = 1
-    capacitance = 600000.0
+    conductance = 100
+    capacitance = 1000000.0
     return foo_temp + (((room_temp - foo_temp) * conductance) / capacitance)
 
 bar = [START_TEMP]
@@ -100,8 +100,8 @@ for i in range(N_ITERATIONS):
     room_temps.append(
         calc_temp(room_temps[-1],
                   heat_in[-1],
-                  heat_out[-1],)
-                  #(bar[-2]-bar[-1]) * 400000.0)
+                  heat_out[-1],
+                  (bar[-2]-bar[-1]) * 1000000.0)
     )
 
 # Print final temp and total energy use.
@@ -112,4 +112,8 @@ print("Final Temp: {0:.2f} C\nEnergy Use: {1:.2f} kJ\nEnergy Loss: {2:.2f} kJ".f
 # Print per iteration values.
 for i in range(len(room_temps)):
     if i % 60 == 0:
-        print("{0}\t| T:{1:.2f}\tQin:{2:.2f}\tQout:{3:.2f} ".format(i // 60, room_temps[i], heat_in[i], heat_out[i]))
+        print("{}\t| T:{:.2f}\tQ:{:.2f}\tQin:{:.2f}\tQout:{:.2f} ".format(i // 60,
+                                                                          room_temps[i],
+                                                                          bar[i] * 1000.0,
+                                                                          heat_in[i],
+                                                                          heat_out[i]))
